52490d5d5f8468a2fb2c5692206b62716ce73047
Lucas Marchisotti <34245219+lmarchisotti@users.noreply.github.com>
2018-08-17 06:56:03 -0300

    Main.c
    Arquivo Main, nele constara as chamadas das funcoes procurando deixa-lo o mais "clean" possivel.

---
+++ Main.c
@@ -0,0 +1,10 @@
+// Aluno: Lucas Marchisotti de Souza            Matricula: 172050060 
+//        Marlon Silveira                                  172050073
+// Trabalho Pratico 1 - Nuvem de pontos
+
+#include"TP1.h"
+
+int main(){
+asd
+as
+}
