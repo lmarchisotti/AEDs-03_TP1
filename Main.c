92d828f84410a53ac18415839c61aabe351c1075
marlonsbasilio <42655521+marlonsbasilio@users.noreply.github.com>
2018-08-27 15:40:37 -0300

    Update Main.c

--- Main.c
+++ Main.c
@@ -5,6 +5,33 @@
 #include"TP1.h"
 
 int main(){
-asd
-as
+
+int i, j, n, A, B;
+PONTO aux;
+
+    FILE *read;
+    read = fopen ("dotscloud.txt","r");
+    if (read == NULL){
+        printf (" ERRO \n");
+        exit (1);
+    }
+    fscanf(read, "%d %d %d", &n, &A, &B);
+    PONTO *leitura = (PONTO*) malloc(sizeof(PONTO)*n);
+
+    printf("Existem %d pontos neste arquivo.\n\n",n);
+    for ( i = 0 ; i < n ; i++){
+        fscanf(read, "%d %d", &leitura[i].x, &leitura[i].y);
+        leitura[i].Ia = ((double) atan (((double)leitura[i].y / (double)(leitura[i].x - A)))*(180/3.141592)); 
+        leitura[i].Ib = ((double) atan (((double)leitura[i].y / (B - (double)leitura[i].x)))*(180/3.141592));
+        leitura[i].Quant = 1;
+    }
+
+    Shellsort(leitura, n);
+    Comparacao(leitura, n);
+
+    printf ("Quantidade máxima de pontos internos: %d", Qmax);
+    printf("\n");
+  
+    fclose(read);
+    return 0;
 }
