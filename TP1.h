d066d4d27bd606cb44b2f0f76496f4c4a2892794
marlonsbasilio <42655521+marlonsbasilio@users.noreply.github.com>
2018-08-27 15:44:13 -0300

    Update TP1.h

--- TP1.h
+++ TP1.h
@@ -3,22 +3,14 @@
 #include<time.h>
 #include<math.h>
 
-struct point{ 
+typedef struct{ 
     int x;
     int y;
     double Ia;      // Inclinacao da aresta que liga o ponto a ancora a
     double Ib;      // Incliancao da aresta que liga o ponto a ancora b
-    int q;          // Contador de pontos internos ao comparado
-}ponto;
-
-struct anchor{
-    int A;
-    int B;
-}ancora;
+    int Quant;      // Contador de pontos internos ao comparado
+}PONTO;
 
 int Qmax;
-
-void Leitura();                         // Funcao para pegar os pontos do arquivo e guardar no programa
-void CInclinacao(ponto *vet);                     // Funcao que calcula a inclinacao da aresta que ligam o ponto a ancora A e B
-void shellsort(ponto *vet, int n);      // Funcao para ordenar de forma decrescente as coordenadas y
-void comparacao();  
+void Shellsort(PONTO *vet, int n);      // Funcao para ordenar de forma decrescente as coordenadas y
+void Comparacao(PONTO *vet, int n);     // Funcao que faz a contagem dos triangulos internos
